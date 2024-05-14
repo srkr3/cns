7a96f09747fe620f6a52fdfd8cf4f589e2677b3a
srkr3 <150950381+srkr3@users.noreply.github.com>
2024-05-14 22:08:46 +0530

    Update HilliCipher.c

--- HilliCipher.java
+++ HilliCipher.java
@@ -1,80 +1,82 @@
-import java.util.*;
+#include <stdio.h>
 
-public class HilliCipher {
-    static float[][] decrypt = new float[3][1];
-    static float[][] a = new float[3][3];
-    static float[][] b = new float[3][3];
-    static float[][] mes = new float[3][1];
-    static float[][] res = new float[3][1];
-    static Scanner sc = new Scanner(System.in);
+float a[3][3];
+float b[3][3];
+float mes[3][1];
+float res_encrypted[3][1];
+float res_decrypted[3][1];
 
-    public static void main(String[] args) {
+void inverse();
 
-        System.out.print("\nEnter a 3 letter string: ");
-        String msg = sc.next();
-        for (int i = 0; i < 3; i++)
-            mes[i][0] = msg.charAt(i) - 97;
-        System.err.println("Enter key matrix of 3x3 matrix: ");
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                a[i][j] = sc.nextFloat();
+int main() {
+    int i, j, k;
+    char msg[4];
 
-        // enc
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 1; j++)
-                for (int k = 0; k < 3; k++) {
-                    res[i][j] = res[i][j] + a[i][k] * mes[k][j];
-                }
-        System.out.print("\nEncrypted string is : ");
-        for (int i = 0; i < 3; i++) {
-            System.out.print((char) (res[i][0] % 26 + 97));
-        }
-        // dec
-        inverse();
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 1; j++)
-                for (int k = 0; k < 3; k++) {
-                    decrypt[i][j] = decrypt[i][j] + b[i][k] * res[k][j];
-                }
-        System.out.print("\nDecrypted string is : ");
-        for (int i = 0; i < 3; i++) {
-            System.out.print((char) (decrypt[i][0] % 26 + 97));
-        }
-        System.out.print("\n");
-    }
-    // inv
+    printf("\nEnter a 3 letter string: ");
+    scanf("%s", msg);
+    for (i = 0; i < 3; i++)
+        mes[i][0] = msg[i] - 'a';
 
-    public static void inverse() {
+    printf("Enter key matrix of 3x3 matrix: ");
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            scanf("%f", &a[i][j]);
 
-        float det = a[0][0] * (a[1][1] * a[2][2] - a[1][2] * a[2][1])
-                - a[0][1] * (a[1][0] * a[2][2] - a[1][2] * a[2][0])
-                + a[0][2] * (a[1][0] * a[2][1] - a[1][1] * a[2][0]);
-
-        if (det == 0) {
-            System.out.println("Matrix is singular, cannot find inverse.");
-        }
-
-        float invDet = 1 / det;
-
-        b[0][0] = (a[1][1] * a[2][2] - a[1][2] * a[2][1]);
-        b[0][1] = (a[0][2] * a[2][1] - a[0][1] * a[2][2]);
-        b[0][2] = (a[0][1] * a[1][2] - a[0][2] * a[1][1]);
-        b[1][0] = (a[1][2] * a[2][0] - a[1][0] * a[2][2]);
-        b[1][1] = (a[0][0] * a[2][2] - a[0][2] * a[2][0]);
-        b[1][2] = (a[0][2] * a[1][0] - a[0][0] * a[1][2]);
-        b[2][0] = (a[1][0] * a[2][1] - a[1][1] * a[2][0]);
-        b[2][1] = (a[0][1] * a[2][0] - a[0][0] * a[2][1]);
-        b[2][2] = (a[0][0] * a[1][1] - a[0][1] * a[1][0]);
-
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++) {
-                b[i][j] = b[i][j] * invDet;
-                if (b[i][j] < 0)
-                    b[i][j] = (b[i][j] % 26) + 26;
-                else
-                    b[i][j] = b[i][j] % 26;
+    // Encryption
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 1; j++)
+            for (k = 0; k < 3; k++) {
+                res_encrypted[i][j] = res_encrypted[i][j] + a[i][k] * mes[k][j];
             }
-
+    printf("\nEncrypted string is : ");
+    for (i = 0; i < 3; i++) {
+        printf("%c", (char)(((int)(res_encrypted[i][0]) % 26) + 'a'));
     }
 
+    // Decryption
+    inverse();
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 1; j++)
+            for (k = 0; k < 3; k++) {
+                res_decrypted[i][j] = res_decrypted[i][j] + b[i][k] * res_encrypted[k][j];
+            }
+    printf("\nDecrypted string is : ");
+    for (i = 0; i < 3; i++) {
+        printf("%c", (char)(((int)(res_decrypted[i][0]) % 26) + 'a'));
+    }
+    printf("\n");
+    return 0;
 }
+
+// inv
+void inverse() {
+    int i, j;
+    float det = a[0][0] * (a[1][1] * a[2][2] - a[1][2] * a[2][1]) -
+                a[0][1] * (a[1][0] * a[2][2] - a[1][2] * a[2][0]) +
+                a[0][2] * (a[1][0] * a[2][1] - a[1][1] * a[2][0]);
+
+    if (det == 0) {
+        printf("Matrix is singular, cannot find inverse.");
+        return;
+    }
+
+    float invDet = 1 / det;
+
+    b[0][0] = (a[1][1] * a[2][2] - a[1][2] * a[2][1]) * invDet;
+    b[0][1] = (a[0][2] * a[2][1] - a[0][1] * a[2][2]) * invDet;
+    b[0][2] = (a[0][1] * a[1][2] - a[0][2] * a[1][1]) * invDet;
+    b[1][0] = (a[1][2] * a[2][0] - a[1][0] * a[2][2]) * invDet;
+    b[1][1] = (a[0][0] * a[2][2] - a[0][2] * a[2][0]) * invDet;
+    b[1][2] = (a[0][2] * a[1][0] - a[0][0] * a[1][2]) * invDet;
+    b[2][0] = (a[1][0] * a[2][1] - a[1][1] * a[2][0]) * invDet;
+    b[2][1] = (a[0][1] * a[2][0] - a[0][0] * a[2][1]) * invDet;
+    b[2][2] = (a[0][0] * a[1][1] - a[0][1] * a[1][0]) * invDet;
+
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++) {
+            b[i][j] = (int)(b[i][j]) % 26;
+            if (b[i][j] < 0)
+                b[i][j] += 26;
+        }
+}
+
